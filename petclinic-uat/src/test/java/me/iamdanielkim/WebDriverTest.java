1ab6920980fdb596b7ad79a18251b619d5ea2911
iamdanielkim <iamdanielkim@sk.com>
2013-11-04 11:06:36 +0900

    uat project based selenium & sauce lab added

---
+++ petclinic-uat/src/test/java/me/iamdanielkim/WebDriverTest.java
@@ -0,0 +1,64 @@
+package me.iamdanielkim;
+
+import com.saucelabs.common.SauceOnDemandAuthentication;
+import com.saucelabs.common.SauceOnDemandSessionIdProvider;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Simple {@link RemoteWebDriver} test that demonstrates how to run your
+ * Selenium tests with <a href="http://saucelabs.com/ondemand">Sauce
+ * OnDemand</a>. *
+ * 
+ * @author Ross Rowe
+ */
+public class WebDriverTest {
+
+	private static final String SAUCE_PROXY_URL = "http://iamdanielkim:3b12cec0-0058-42d5-a79d-d56a834e8d22@localhost:4445/wd/hub";
+	private static final String SAUCELAB_URL = "http://iamdanielkim:3b12cec0-0058-42d5-a79d-d56a834e8d22@ondemand.saucelabs.com:80/wd/hub";
+	private WebDriver driver;
+
+	@Before
+	public void setUp() throws Exception {
+
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		capabilities.setCapability("version", "17");
+		capabilities.setCapability("platform", Platform.XP);
+		this.driver = new RemoteWebDriver(new URL(SAUCE_PROXY_URL),
+				capabilities);
+	}
+
+	@Ignore
+	@Test
+	public void webDriver() throws Exception {
+		driver.get("http://www.amazon.com/");
+		assertEquals(
+				"Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more",
+				driver.getTitle());
+	}
+
+	@Test
+	public void goPetClinic() throws Exception {
+		driver.get("http://127.0.0.1:9966/petclinic/");
+		assertEquals("PetClinic :: a Spring Framework demonstration",
+				driver.getTitle());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
+
+}
